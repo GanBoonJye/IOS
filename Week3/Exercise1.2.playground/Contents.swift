74c2f02551c92b4ca79920032c3c8e8a165b620f
Gan Boon Jye <ganboonjye0312@e.neivce.edu.my>
2024-11-20 09:32:03 +0800

    Exercise1.2 Done

--- Week3/Exercise1.2.playground/Contents.swift
+++ Week3/Exercise1.2.playground/Contents.swift
@@ -1,3 +1,16 @@
 import UIKit
 
+var time = 3
 
+if time >= 5 && time <= 11{
+    print("Good Morning!")
+}
+else if time >= 12 && time <= 17 {
+    print("Good Afternoon!")
+}
+else if time >= 18 && time <= 21 {
+    print("Good Evening!")
+}
+else {
+    print("Good Night")
+}
