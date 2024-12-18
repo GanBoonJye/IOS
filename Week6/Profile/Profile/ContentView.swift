cb4ccc4e94a8824f6003acd8d1fe73df91591d0b
Gan Boon Jye <ganboonjye0312@e.neivce.edu.my>
2024-12-18 11:14:46 +0800

    Week 7 Done

--- Week6/Profile/Profile/ContentView.swift
+++ Week6/Profile/Profile/ContentView.swift
@@ -9,13 +9,48 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack{
+            Image("Back")
+                .resizable()
+                .ignoresSafeArea()
+            VStack{
+                Image("ganboonjye")
+                    .resizable()
+                    .scaledToFit()
+                    .aspectRatio(contentMode:.fit)
+                    .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                    .frame(width:  200.0 ,height: 200.0)
+                Text("GanBoonJye")
+                    .font(.largeTitle)
+                    .bold()
+                Text("IOS Developer")
+                    .font(.title)
+                Text("Hi,My name is Gan Boon Jye 17 years old. I like to play games")
+                    .multilineTextAlignment(.center)
+                    .padding()
+                Spacer()
+                    HStack{
+                        Image("ig")
+                            .resizable()
+                            .aspectRatio(contentMode:.fit)
+                            .padding(.all)
+                        Image("fb")
+                            .resizable()
+                            .aspectRatio(contentMode:.fit)
+                            .padding(.all)
+                        Image("in")
+                            .resizable()
+                            .aspectRatio(contentMode:.fit)
+                            .padding(.all)
+                        Image("youtube")
+                            .resizable()
+                            .aspectRatio(contentMode:.fit)
+                            .padding(.all)
+            
+                }
+                    .padding()
+            }
         }
-        .padding()
     }
 }
 
