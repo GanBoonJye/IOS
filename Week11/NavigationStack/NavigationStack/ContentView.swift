5e48156b3eec56df3ddbf2947ff774cf3389931c
Gan Boon Jye <ganboonjye0312@e.neivce.edu.my>
2025-01-22 11:32:28 +0800

    week 11 all done

--- Week11/NavigationStack/NavigationStack/ContentView.swift
+++ Week11/NavigationStack/NavigationStack/ContentView.swift
@@ -8,14 +8,56 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var searchText = " "
+    @State private var isSheetPresented = false
+    let Asia = ["Kuala Lumpur","Tokyo","Korea"]
+    let Europe = ["London","New York","Paris"]
+    
+ 
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack{
+            List{
+                Section(header:Text("Asia")) {
+                    ForEach(Asia,id:\.self) { Asia in
+                        NavigationLink {
+                            Text("Item Detail \(Asia)")
+                        }label:{
+                            Text(Asia)
+                        }
+                    }
+                }
+                Section(header:Text("Europe")) {
+                    ForEach(Europe,id:\.self) { Europe in
+                        NavigationLink {
+                            Text("Item Detail \(Europe)")
+                        }label:{
+                            Text(Europe)
+                        }
+                        
+                    }
+                }
+                
+            }
+            .navigationTitle("City")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button(action: {
+                        isSheetPresented = true
+                    }){
+                        Image(systemName: "info.circle")
+                            .font(.system(size:30))
+                    }
+                }
+            }
+            .sheet(isPresented:$isSheetPresented){
+                VStack{
+                    Text("This app was created by GBJ")
+                        .presentationDetents([.height(300),.large])
+                        .font(.headline)
+                        .padding()
+                }
+            }
         }
-        .padding()
     }
 }
 
