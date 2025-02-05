398c9c102158b7806a6869b558ffbf20e1949f92
Gan Boon Jye <ganboonjye0312@e.neivce.edu.my>
2025-02-05 11:34:56 +0800

    week12 done

--- Week12/OrderingApp/OrderingApp/DrinkView.swift
+++ Week12/OrderingApp/OrderingApp/DrinkView.swift
@@ -1,18 +1,18 @@
 //
-//  Weather.swift
-//  NavigationStack
+//  DrinkView.swift
+//  OrderingApp
 //
-//  Created by labuser on 22/01/2025.
+//  Created by labuser on 05/02/2025.
 //
 
 import SwiftUI
 
-struct Weather: View {
+struct DrinkView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    Weather()
+    DrinkView()
 }
